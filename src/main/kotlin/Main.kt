ddb32e440651f6b5e5d275f9d6ec9674eb14fe15
Shreya1050 <shreya.aggarwal@fretron.com>
2024-02-08 15:55:08 +0530

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,8 @@ import org.bson.Document
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val connectionString = ConnectionString("mongodb://localhost:27017")
-    val client = MongoClients.create(connectionString)
-    val database: MongoDatabase = client.getDatabase("students")
-    val collection: MongoCollection<Document> = database.getCollection("students.studentData")
+//    val connectionString = ConnectionString("mongodb://localhost:27017")
+//    val client = MongoClients.create(connectionString)
+//    val database: MongoDatabase = client.getDatabase("students")
+//    val collection: MongoCollection<Document> = database.getCollection("students.studentData")
 }
